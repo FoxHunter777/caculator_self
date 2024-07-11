f6016276206901a177b5b9455f687d109adf6df9
eunsu <lesljk@naver.com>
2024-07-11 15:17:28 +0900

    first commit

--- caculator_self/View/CalculatorView.swift
+++ caculator_self/View/CalculatorView.swift
@@ -9,43 +9,108 @@ import SwiftUI
 
 
 struct CalculatorView: View {
-    var tmpName:String?
-    var tmpPrice:Int?
-    var tmpweight:Int?
-    var tmpQuantity:Int?
+    @State var gprice: String = "0"
+    @State var qprice: String = "0"
+    
+    @State var tmpName:String = ""
+    @State var tmpPrice:String = ""
+    @State var tmpweight:String = ""
+    @State var tmpQuantity:String = ""
     
     var body: some View {
         VStack{
-            HStack{
-                Text("여기에 제품명 입력")
-                RoundedRectangle(cornerRadius: 5)
-                    .frame(width: 20, height: 20)
-                    .foregroundColor(.blue)
-            }
-            
-            HStack{
-                Text("여기에 제품 가격입력")
-            }
-            
-            HStack{
-                Text("제품 무게와 제품 개수")
-            }
-            
             VStack{
-                Button(action: {
-                    
-                }, label: {
-                    Text("계산하기")
-                        .foregroundColor(Color.green)
-                })
                 
-                Button(action: {
+                HStack {
+                    Text("g당 ")
+                        .font(.largeTitle)
                     
-                }, label: {
-                    Text("장바구니 추가")
-                        .foregroundColor(Color.purple)
-                })
-            }.padding(.top, 50)
+                    Spacer()
+                    
+                    Text(gprice)
+                        .font(.largeTitle)
+                    Text("원")
+                        .font(.largeTitle)
+                }
+
+                HStack {
+                    Text("개당 ")
+                        .font(.largeTitle)
+                    
+                    
+                    Spacer()
+                    Text(qprice)
+                        .font(.largeTitle)
+                    Text("원")
+                        .font(.largeTitle)
+                }
+                
+            }.padding(.bottom, 50)
+                .padding(.horizontal, 70)
+            
+            VStack(alignment: .center) {
+                HStack{
+                    TextField("제품명 입력", text: $tmpName)
+                }
+                .padding(10)
+                .border(Color.black)
+                
+                HStack{
+                    TextField("제품 가격 입력", text: $tmpPrice)
+                }
+                .padding(10)
+                .border(Color.black)
+                
+                HStack{
+                    HStack {
+                        TextField("제품 무게 입력", text: $tmpweight)
+                    }
+                    .padding(10)
+                    .border(Color.black)
+                    HStack {
+                        TextField("제품 개수 입력", text: $tmpQuantity)
+                    }
+                    .padding(10)
+                    .border(Color.black)
+                }
+                
+                
+                
+            }.padding(.horizontal, 70)
+            
+            
+            
+//            Spacer()
+            VStack{
+                HStack {
+                    Button(action: {
+                        var g = Float(tmpPrice)! / Float(tmpweight)! * 100
+                        var q = Float(tmpPrice)! / Float(tmpQuantity)!
+                        
+                        gprice = "\(round(g))"
+                        qprice = "\(round(q))"
+                    }, label: {
+                        Text("계산하기")
+                            .foregroundColor(Color.white)
+                    })
+                    .padding(.horizontal, 21.0)
+                    .padding(.vertical, 5)
+                }
+                .background(Color.green)
+                
+                
+                HStack {
+                    Button(action: {
+                        
+                    }, label: {
+                        Text("장바구니 추가")
+                            .foregroundColor(Color.white)
+                }).padding(5)
+                }
+                .background(Color.green)
+            }
+            .padding(.top, 50)
+            
         }
     }
         
--- caculator_self/View/CompareView.swift
+++ caculator_self/View/CompareView.swift
@@ -8,8 +8,31 @@
 import SwiftUI
 
 struct CompareView: View {
+    
+    //목록을 1부터 1000까지 만듬
+        let data = Array(1...20).map { "목록 \($0)"}
+        
+        //화면을 그리드형식으로 꽉채워줌
+        let columns = [
+            GridItem(.flexible()),
+            GridItem(.flexible())
+        ]
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView {
+            LazyVGrid(columns: columns, spacing: 20) {
+                ForEach(data, id: \.self) {i in
+                    VStack {
+                       Capsule()
+                           .fill(Color.green)
+                           .frame(height: 150)
+                       Text(i)
+                           .foregroundColor(.secondary)
+                   }
+                }
+            }
+            .padding(.horizontal)
+        }
     }
 }
 
